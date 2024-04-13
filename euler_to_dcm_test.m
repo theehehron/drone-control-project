ec1ddb61417dcb971445826b54f06613d22492ca
Aaron Greisen <aaron.greisen1@gmail.com>
2024-04-13 10:42:02 -0700

    Adjusted random tests

--- euler_to_dcm_test.m
+++ euler_to_dcm_test.m
@@ -1,15 +1,15 @@
 clear; close all; clc;
 
 tolerance = 0.01;
-order_of_mag = .001;
+order_of_mag = .00001;
 test_count = 0;
 fail_count = 0;
 for mags = 1:6
-    for testseq = 1:50
+    for testseq = 1:800
         test_count = test_count + 1;
-        phi = 2*pi*(rand(1, 1)-1/2);
-        theta = 2*pi*(rand(1, 1)-1/2);
-        psi = 2*pi*(rand(1, 1)-1/2);
+        phi = 2*pi*(rand(1, 1)-1/2)*order_of_mag;
+        theta = 2*pi*(rand(1, 1)-1/2)*order_of_mag;
+        psi = 2*pi*(rand(1, 1)-1/2)*order_of_mag;
 
         C1 = angle2dcm(psi, theta, phi);
         C2 = euler_to_dcm([phi, theta, psi]);
@@ -19,7 +19,10 @@ for mags = 1:6
                 if (C1(i, j) - C2(i, j))/C1(i, j) > tolerance
                     warning(strcat("test failed at order of mag: ", num2str(order_of_mag)));
                     disp(C1-C2)
-                    error_percent = min(min(C1(i, j)))/max(max(C1(i, j) - C2(i, j)));
+                    disp(strcat("phi = ", num2str(phi)))
+                    disp(strcat("theta = ", num2str(theta)))
+                    disp(strcat("psi = ", num2str(psi)))
+                    error_percent = (C1(i, j) - C2(i, j))/C1(i, j)*100;
                     disp(strcat("C1(i, j) is: ", num2str(C1(i, j))))
                     disp(strcat("error is: ", num2str(C1(i, j)-C2(i, j))))
                     disp(strcat("Error is ", num2str(error_percent), "%"))
--- matrix_multiply_3x3_test.m
+++ matrix_multiply_3x3_test.m
@@ -5,7 +5,7 @@ order_of_mag = .001;
 test_count = 0;
 fail_count = 0;
 for mags = 1:6
-    for testseq = 1:50
+    for testseq = 1:800
         test_count = test_count + 1;
         A = 2.*(rand(3, 3)-1/2)*order_of_mag;
         B = 2.*(rand(3, 3)-1/2)*order_of_mag;
@@ -18,7 +18,7 @@ for mags = 1:6
                 if (C1(i, j) - C2(i, j))/C1(i, j) > tolerance
                     warning(strcat("test failed at order of mag: ", num2str(order_of_mag)));
                     disp(C1-C2)
-                    error_percent = min(min(C1(i, j)))/max(max(C1(i, j) - C2(i, j)));
+                    error_percent = (C1(i, j) - C2(i, j))/C1(i, j)*100;
                     disp(strcat("C1(i, j) is: ", num2str(C1(i, j))))
                     disp(strcat("error is: ", num2str(C1(i, j)-C2(i, j))))
                     disp(strcat("Error is ", num2str(error_percent), "%"))
