0f6876b38da963b7558e7ee2649afc95d7470c19
Aaron Greisen <aaron.greisen1@gmail.com>
2024-04-15 20:40:21 -0700

    first commit of arduino implementation

--- buildmex.m
+++
@@ -1,6 +0,0 @@
-clear; close all; clc;
-
-mex matrix_multiply_3x3.cpp attitude_representation.cpp
-mex euler_to_dcm.cpp attitude_representation.cpp
-mex dcm_to_quat.cpp attitude_representation.cpp
-mex dcm_to_euler.cpp attitude_representation.cpp
