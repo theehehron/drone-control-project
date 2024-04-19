fb2ebd61fbb9c93a7346d667169e0eef09005949
Aaron Greisen <aaron.greisen1@gmail.com>
2024-04-18 23:12:35 -0700

    messed around a bit more with arduino code and learned a lot

--- attitude_representation_ino.ino
+++ attitude_representation_ino.ino
@@ -1,27 +1,17 @@
 #include "attitude_representation.hpp"
 #include "Wire.h"
+#include "AGmpu6050.hpp"
+
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050
+int16_t gyro_x, gyro_y, gyro_z;
+std::vector<int16_t> gyrocals(3);
+
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   std::vector<float> eulers = {-3.0854, -0.9146, 1.7651};
   Matrix Cbv = euler_to_dcm(eulers);
-
-  Serial.print(Cbv[0][0], 4);
-  Serial.print(" ");
-  Serial.print(Cbv[0][1], 4);
-  Serial.print(" ");
-  Serial.println(Cbv[0][2], 4);  
-  Serial.print(Cbv[1][0], 4);
-  Serial.print(" ");
-  Serial.print(Cbv[1][1], 4);
-  Serial.print(" ");
-  Serial.println(Cbv[1][2], 4); 
-  Serial.print(Cbv[2][0], 4);
-  Serial.print(" ");
-  Serial.print(Cbv[2][1], 4);
-  Serial.print(" ");
-  Serial.println(Cbv[2][2], 4); 
 
   unsigned long a = millis();
   delay(5000);
@@ -32,12 +22,45 @@ void setup() {
   float y1 = 13.78;
 
   float out = trap_integration(y0, y1, delta_t);
-  Serial.println(out, 5);
-  
+//  Serial.println(out, 5);
+
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(B00000000);
+//  Wire.write(0x1B);
+//  Wire.write(B00000000);
+  Wire.endTransmission(true);
+
+  gyrocals = gyro_calibration(MPU_ADDR);
+  Serial.print(gyrocals[0]);
+  Serial.print(" ");
+  Serial.print(gyrocals[1]);
+  Serial.print(" ");
+  Serial.print(gyrocals[2]);
 }
 
 
 
 void loop() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x43); // MPU-6050 Register Map and Descriptions
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 3*2, true);
+  gyro_x = ((Wire.read()<<8 | Wire.read()) - gyrocals[0])/131;
+  gyro_y = ((Wire.read()<<8 | Wire.read()) - gyrocals[1])/131;
+  gyro_z = ((Wire.read()<<8 | Wire.read()) - gyrocals[2])/131;
 
+  Serial.print(gyro_x);
+  Serial.print(", ");
+  Serial.print(gyro_y);
+  Serial.print(", ");
+  Serial.print(gyro_z);
+  Serial.println();
+  delay(3);
+//  Serial.print(gyrocals[0]);
+//  Serial.print(" ");
+//  Serial.print(gyrocals[1]);
+//  Serial.print(" ");
+//  Serial.println(gyrocals[2]);
 }
